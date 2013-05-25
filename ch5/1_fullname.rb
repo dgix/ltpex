a254fc130c7a21576796252523804b9c0bf82c0c
David Gonzalez <dg@ix.fake>
2013-05-25 01:24:22 -0400

    Ch5, added solutions

--- ch5/1_fullname.rb
+++ ch5/1_fullname.rb
@@ -8,3 +8,12 @@ puts 'What is your last name?'
 last = gets.chomp
 
 puts 'Greetings, ' + first + ' ' + middle + ' ' + last + '.'
+
+# solution:
+# puts "What's your first name?"
+# f_name = gets.chomp
+# puts "What's your middle name?"
+# m_name = gets.chomp
+# puts "What's your last name?"
+# l_name = gets.chomp
+# puts "Chris Pine is cooler than you, #{f_name} #{m_name} #{l_name}."
--- ch5/2_favnum.rb
+++ ch5/2_favnum.rb
@@ -4,3 +4,13 @@ favnum = gets.chomp
 newnum = favnum.to_i + 1
 
 puts 'Your bigger, better favorite number should be ' + newnum.to_s + '.'
+
+# solution:
+# puts 'Hey!  What\'s your favorite number?'
+# fav_num = gets.chomp.to_i
+# better_num = fav_num + 1
+# puts 'That\'s ok, I guess, but isn\'t '+better_num.to_s+' just a bit better?'
+# or
+# puts "Hey!  What's your favorite number?"
+# fav_num = gets.chomp.to_i
+# puts "That's ok, I guess, but isn't #{fav_num + 1} just a bit better?"
