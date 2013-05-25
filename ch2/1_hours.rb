9a242fa49d10343b35f1c2342b08afe32a3a8a2c
David Gonzalez <dg@ix.fake>
2013-05-25 01:11:33 -0400

    Corrections, added solutions

--- ch2/1_hours.rb
+++ ch2/1_hours.rb
@@ -1,1 +1,5 @@
 puts 365 * 24
+
+# solution:
+# puts 24*365
+# puts "(or #{24*366} on occasion)"
--- ch2/3_seconds.rb
+++ ch2/3_seconds.rb
@@ -1,2 +1,6 @@
 puts 39 * 365 * 24 * 60 * 60
 
+# solution:
+# puts 60*60*24*(365*32 + 9)
+# or:
+# puts(Time.new - Time.gm(1976, 8, 3, 13, 31))
