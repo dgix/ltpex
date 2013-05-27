e0f8fa4a4579012261fc6113e91c3a57ebbd4bc5
David Gonzalez <dg@ix.fake>
2013-05-27 19:16:30 -0400

    Ch6, corrections, added solutions

--- ch6/1_boss.rb
+++ ch6/1_boss.rb
@@ -1,3 +1,9 @@
 puts 'What do you want?'
-req = gets.chomp!
+req = gets.chomp
 puts 'WHADDAYA MEAN "' + req.upcase + '"?!? YOU\'RE FIRED!!'
+
+# solution:
+# names = ['johnson', 'smith', 'weinberg', 'filmore']
+# puts "CAN'T YOU SEE I'M BUSY?!  MAKE IT FAST, #{names[rand(4)].upcase}!"
+# request = gets.chomp
+# puts "WHADDAYA MEAN \"#{request.upcase}\"?!? YOU'RE FIRED!!"
--- ch6/2_contents.rb
+++ ch6/2_contents.rb
@@ -6,3 +6,15 @@ puts (        'Chapter 2:  Numbers'.ljust(line_width*0.6) + 'page  9'.rjust(line
 puts (        'Chapter 3:  Letters'.ljust(line_width*0.6) + 'page 13'.rjust(line_width*0.4))
 
 # The line_width*decimal kludge came about after peeking at the solution and wanting to be different
+
+# solution:
+# title  = 'Table of Contents'.center(50)
+# chap_1 = 'Chapter 1:  Getting Started'.ljust(30) + 'page  1'.rjust(20)
+# chap_2 = 'Chapter 2:  Numbers'.ljust(30)         + 'page  9'.rjust(20)
+# chap_3 = 'Chapter 3:  Letters'.ljust(30)         + 'page 13'.rjust(20)
+
+# puts title
+# puts
+# puts chap_1
+# puts chap_2
+# puts chap_3
