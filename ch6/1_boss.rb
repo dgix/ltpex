addad0385d5b646f1977b03e7547cc4dd2650448
David Gonzalez <dg@ix.fake>
2013-05-27 19:08:01 -0400

    Ch6, first try

---
+++ ch6/1_boss.rb
@@ -0,0 +1,3 @@
+puts 'What do you want?'
+req = gets.chomp!
+puts 'WHADDAYA MEAN "' + req.upcase + '"?!? YOU\'RE FIRED!!'
---
+++ ch6/2_contents.rb
@@ -0,0 +1,8 @@
+line_width = 50
+puts ('Table of Contents'.center(line_width))
+puts ''
+puts ('Chapter 1:  Getting Started'.ljust(line_width*0.6) + 'page  1'.rjust(line_width*0.4))
+puts (        'Chapter 2:  Numbers'.ljust(line_width*0.6) + 'page  9'.rjust(line_width*0.4))
+puts (        'Chapter 3:  Letters'.ljust(line_width*0.6) + 'page 13'.rjust(line_width*0.4))
+
+# The line_width*decimal kludge came about after peeking at the solution and wanting to be different
